74fa112b8f0d65eb6e5da311e04affc14c2b17c8
Marcos Sierra <sierramarcos146@gmail.com>
2023-03-14 17:11:09 -0600

    agregando codigo

---
+++ src/main/java/org/kurokami/prueba/PruebaApplication.java
@@ -0,0 +1,21 @@
+package org.kurokami.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PruebaApplication extends SpringBootServletInitializer{
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(PruebaApplication.class);
+	}
+
+}
